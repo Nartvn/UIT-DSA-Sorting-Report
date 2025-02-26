67e46010ffef87cca5d8f6f0fe0b9940669a27b0
Nartvn <nghiabom9999@gmail.com>
2025-02-26 22:53:39 +0700

    v1.2

--- Sort(c++)/maincode.cpp
+++ Sort(c++)/maincode.cpp
@@ -1,9 +1,22 @@
 #include "bits/stdc++.h"
-
+#include <chrono>
 using namespace std;
-
+using namespace std::chrono;
 vector < int > endtime;
 vector < int > vi;
+
+int SNC(string x)
+{
+    bool ok = 0;
+    if (x[0] == '-') ok = 1;
+    else x = ' ' + x; 
+    int num = 0;
+    for (int i = 1 ; i < (int)x.size() ; ++i)
+        num = num * 10 + (int)(x[i] - '0');
+    if (ok) num = -num;
+    return num;
+}
+
 void Get(int number)
 {
     string name = "test";
@@ -12,8 +25,26 @@ void Get(int number)
 
     vi.clear();
 
-    
+    ifstream nowfile(name);
 
+    string num;
+    while (getline(nowfile, num))
+    {
+        vi.push_back(SNC(num));
+    }
+    
+    nowfile.close();
+
+    auto start = high_resolution_clock::now(); /// get starting timepoint
+    ///----sorting here-----
+    sort(vi.begin(), vi.end());
+    //----------------------
+    auto stop = high_resolution_clock::now(); /// end timepoint
+
+    auto duration = duration_cast<microseconds>(stop - start);
+    int end = duration.count() / 1000;
+
+    endtime.push_back(end);
     return;
 }
 
@@ -29,14 +60,21 @@ void PrintfTime()
     ofstream myfile;
     myfile.open ("Report.txt");
     for (int i = 0 ; i <= 9 ; ++i)
-        cout << "Time to run test " << i << " is: " << endtime[i] << '\n';
+        myfile << "Time to run test " << i << " is: " << endtime[i] << '\n';
+
+    int average = 0;
+    for (int i = 0 ; i <= 9 ; ++i)
+        average +=  endtime[i];
+    average /= 9;
+    myfile << "average is: " << average;
+    
     myfile.close();
     return;
     
 }
 int main()
 {
-    //GetTime();
-    //PrintfTime();
+    GetTime();
+    PrintfTime();
     return 0;
 }
